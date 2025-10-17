14aa930ee5f5502ea4e4b7fe46fdb463a1652292
jetlee <jetlee@email.com>
2025-10-17 10:32:43 -0500

    Agrego offline.html para que muestre si no hay internet

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -4,21 +4,32 @@ self.addEventListener("install", (event) => {
       return cache.addAll([
         "/",
         "/index.html",
-        "/css/estilos.css",
+        "/login.html",
+        "/offline.html",
+        "/css/nav.css",
+        "/css/offline.css",
         "/js/pwa-install.js",
         "/manifest.json",
         "/assets/icon-192x192.png",
-        "/assets/icon-512x512.png"
+        "/assets/icon-512x512.png",
+        "/assets/img/logoCoffee.png"
       ]);
     })
   );
   console.log("✅ Service Worker instalado (Café Ágora)");
 });
 
+// Network First: intenta cargar de la red primero
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        // Si hay internet, devolvemos la respuesta de la red
+        return response;
+      })
+      .catch(() => {
+        // Si no hay internet, mostramos offline.html
+        return caches.match("/offline.html");
+      })
   );
 });
